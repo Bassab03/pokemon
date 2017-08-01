31736ac6ec906035041ea50f3466d6272c70b20f
Bassab03 <bas.de.bue@edpnet.be>
2017-08-01 15:26:46 +0200

    Updated to use jQuery

--- js/iv.js
+++ js/iv.js
@@ -1,33 +1,34 @@
-addEventListener("load", init, false);
-
 var level = 0;
 var baseStat = 0;
 var ev = 0;
 var total = 0;
 var iv = 0;
 
-var levelInput;
-var baseStatInput;
-var evInput;
-var totalInput;
-
-function init() {
-  levelInput = document.querySelector("#levelInput");
-  baseStatInput = document.querySelector("#baseStatInput");
-  evInput = document.querySelector("#evInput");
-  totalInput = document.querySelector("#totalInput");
-  submitButton = document.querySelector("#submitButton");
-  submitButton.addEventListener("click", calculateIVs, false);
-}
+$("#calcButton").click(function() {
+  calculateIVs();
+});
 
 function calculateIVs() {
   getUserInput();
-  iv = 2 * (total - (((2 * baseStat) + (ev / 4)) * (level / 100) + 10 + level));
+  if ($("#selectStat").value == "hp") {
+    iv = Math.floor(2 * (total - (((2 * baseStat) + (ev / 4)) * (level / 100) + 10 + level)));
+  } else {
+    iv = Math.floor();
+  }
+  $("#output").textContent = updateMessage();
 }
 
 function getUserInput() {
-  level = levelInput.value;
-  baseStat = baseStatInput.value;
-  ev = evInput.value;
-  total = totalInput.value;
+  level = $("#levelInput").value;
+  baseStat = $("#baseStatInput").value;
+  ev = $("#evInput").value;
+  total = $("#totalInput").value;
+}
+
+function updateMessage() {
+  if (iv < 0 || iv > 31) {
+    return "Whoops, looks like something went wrong. Please verify your values.";
+  } else {
+    return "Your Pokémon has " + iv + " IVs in the specified stat.";
+  }
 }
