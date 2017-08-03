5edf6d2e3488f6f7a86e2be13f9e39649c43d1a6
Bassab03 <bas.de.bue@edpnet.be>
2017-08-03 10:37:07 +0200

    IV Calculator now fully functional

--- js/iv.js
+++ js/iv.js
@@ -4,65 +4,105 @@ var ev = 0;
 var total = 0;
 var minIV = 0;
 var maxIV = 0;
+var selectedStat = 0;
+var selectedNature = 0;
 
-$(document).ready(function() {
-  $("#calcButton").click(function() {
-    calculateIVs();
-  });
-});
+addEventListener("load", init, false);
+
+const natureArray = [
+    ["Hardy", 100, 100, 100, 100, 100],
+    ["Lonely", 110, 90, 100, 100, 100],
+    ["Brave", 110, 100, 100, 100, 90],
+    ["Adamant", 110, 100, 90, 100, 100],
+    ["Naughty", 110, 100, 100, 90, 100],
+    ["Bold", 90, 110, 100, 100, 100],
+    ["Docile", 100, 100, 100, 100, 100],
+    ["Relaxed", 100, 110, 100, 100, 90],
+    ["Impish", 100, 110, 90, 100, 100],
+    ["Lax", 100, 110, 100, 90, 100],
+    ["Timid", 90, 100, 100, 100, 110],
+    ["Hasty", 100, 90, 100, 100, 110],
+    ["Serious", 100, 100, 100, 100, 100],
+    ["Jolly", 100, 100, 90, 100, 110],
+    ["Naive", 100, 100, 100, 90, 110],
+    ["Modest", 90, 100, 110, 100, 100],
+    ["Mild", 100, 90, 110, 100, 100],
+    ["Quiet", 100, 100, 110, 100, 90],
+    ["Bashful", 100, 100, 100, 100, 100],
+    ["Rash", 100, 100, 110, 90, 100],
+    ["Calm", 90, 100, 100, 110, 100],
+    ["Gentle", 100, 90, 100, 110, 100],
+    ["Sassy", 100, 100, 100, 110, 90],
+    ["Careful", 100, 100, 90, 110, 100],
+    ["Quirky", 100, 100, 100, 100, 100]
+];
+
+function init() {
+    document.querySelector("#selectStat").addEventListener("change", selectStatChanged, false);
+    document.querySelector("#selectNature").addEventListener("change", selectNatureChanged, false);
+    document.querySelector("#calcButton").addEventListener("click", calculateIVs, false);
+}
 
 function calculateIVs() {
-  getUserInput();
-  if (document.getElementById("selectStat").value == "hp") {
-    // iv = Math.floor(2 * (total - (((2 * baseStat) + (ev / 4)) * (level / 100) + 10 + level)));
-    minIV = Math.ceil((total - level - 10 - 2 * baseStat * level / 100 - ev * level / 400) * 100 / level)
-    maxIV = Math.floor((Number(total) + 0.999 - level - 10 - 2 * baseStat * level / 100 - ev * level / 400) * 100 / level)
-    // total = Math.floor((2 * baseStat + iv + ev / 4) * level / 100 + level + 10)
-    console.log("total + 0.999 = " + Number(total) + 0.999);
-    console.log("total + 0.999 - level - 10 = " + total - level + 0.999 - 10);
-    console.log("total + 0.999 - level - 10 - 2 * baseStat * level / 100 = " + total +  0.999 - level - 10 - 2 * baseStat * level / 100);
-    console.log("total + 0.999 - level - 10 - 2 * baseStat * level / 100 - ev * level / 400 = " + total +  0.999 - level - 10 - 2 * baseStat * level / 100 - ev * level / 400);
-    console.log("(total + 0.999 - level - 10 - 2 * baseStat * level / 100 - ev * level / 400) * 100 / level = " + (total +  0.999 - level - 10 - 2 * baseStat * level / 100 - ev * level / 400) * 100 / level);
-    console.log("Finished HP IV calculation. The result lies between " + minIV + " and " + maxIV);
-  } else {
-    iv = Math.floor();
-    console.log("Finished main IV calculation. The result is " + iv);
-  }
-  document.querySelector("#output").innerHTML = checkNumbers(minIV, maxIV, 0, 31);
+    document.querySelector("#output").innerHTML = tryIVs(0, 31);
 }
 
 function getUserInput() {
-  level = document.getElementById("levelInput").value;
-  baseStat = document.getElementById("baseStatInput").value;
-  ev = document.getElementById("evInput").value;
-  total = document.getElementById("totalInput").value;
-  console.log("Fetched user values: " + level + " (level), " + baseStat + " (base stat), " + ev + " (EVs), " + total + " (total), ");
+    level = Number(document.querySelector("#levelInput").value);
+    baseStat = Number(document.querySelector("#baseStatInput").value);
+    ev = Number(document.querySelector("#evInput").value);
+    total = Number(document.querySelector("#totalInput").value);
 }
 
-function updateMessage() {
-  if (minIV < 0 || minIV > 31 || isNaN(minIV)) {
-    return "Whoops, looks like something went wrong.<br/>Please verify your values.";
-  } else if (minIV == maxIV) {
-    return "Your Pokémon has " + minIV + " IVs in the specified stat.";
-  } else {
-    return "Your Pokémon has " + minIV + " - " + maxIV + " IVs in the specified stat.";
-  }
+function tryIVs(min, max) {
+    getUserInput();
+    let minTryIV = -1;
+    let maxTryIV = -1;
+    if (document.querySelector("#selectStat").value == 0) {
+        for (let i = min; i <= max; i++) {
+            if (Math.floor(((i + 2 * baseStat + ev / 4) * level / 100) + 10 + level) == total) {
+                if (minTryIV == -1) {
+                    minTryIV = i;
+                    maxTryIV = i;
+                } else {
+                    maxTryIV = i;
+                }
+            }
+        }
+    } else {
+        for (let i = min; i <= max; i++) {
+            if (Math.floor(Math.floor(((2 * baseStat + i + ev / 4) * level) / 100 + 5) * getNatureMultiplier()) == total) {
+                if (minTryIV == -1) {
+                    minTryIV = i;
+                    maxTryIV = i;
+                } else {
+                    maxTryIV = i;
+                }
+            }
+        }
+    }
+    if (!isFinite(minTryIV) || !isFinite(maxTryIV) || minTryIV == -1 || maxTryIV == -1) {
+        return "Whoops, looks like something went wrong.<br/>Please verify your values.";
+    } else if (minTryIV == maxTryIV) {
+        return "Your Pok&eacute;mon has " + minTryIV + " IVs in the specified stat.";
+    } else {
+        return "Your Pok&eacute;mon has " + minTryIV + " - " + maxTryIV + " IVs in the specified stat.";
+    }
 }
 
-function checkNumber(value, min, max) {
-  if (value < min) return min;
-  if (value > max) return max;
-  return value;
+function selectStatChanged(event) {
+    selectedStat = event.target.value;
+    if (selectedStat != 0) {
+        document.querySelector("#selectNatureDiv").classList.remove("selectNature");
+    } else {
+        document.querySelector("#selectNatureDiv").classList.add("selectNature");
+    }
 }
 
-function checkNumbers(valueMin, valueMax, min, max) {
-  let vMin = checkNumber(valueMin, min, max);
-  let vMax = checkNumber(valueMax, min, max);
-  if (isNaN(vMin) || isNaN(vMax)) {
-    return "Whoops, looks like something went wrong.<br/>Please verify your values.";
-  } else if (vMin == vMax) {
-    return "Your Pokémon has " + vMin + " IVs in the specified stat.";
-  } else {
-    return "Your Pokémon has " + vMin + " - " + vMax + " IVs in the specified stat.";
-  }
+function selectNatureChanged(event) {
+    selectedNature = event.target.value;
+}
+
+function getNatureMultiplier() {
+    return natureArray[selectedNature][selectedStat] / 100;
 }
